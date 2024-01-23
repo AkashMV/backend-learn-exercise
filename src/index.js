350acd8e44580ca2af14e380876c6dfe638e1cc3
Akash <naduaku7@gmail.com>
2024-01-23 23:33:09 +0530

    continuation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import dotenv from 'dotenv'
 import connectDB from "./db/index.js"
+import express from 'express'
 
+const app = express()
 dotenv.config({
     path: "./env"
 })
+
 
 connectDB()
 .then(() => {
@@ -13,33 +16,5 @@ connectDB()
 })
 .catch((error) => {
     console.log(`MONGO DB CONN FAILED : ${error}`);
+    process.exit(1)
 })
-//Straightforward approach
-//creating a function
-// function connectDB(){
-
-// }
-//then calling it..
-// connectDB()
-//we can improve this
-//we will use async await and IIFE functions to establish database connectivity
-//we import express and establish a server connection
-// const app = express()
-// (async () => {
-//     try{     await mongoose.connect(`${process.env.MONGO_URL}/${DB_
-//    NAME}`)
-//         app.on("error", (error)=>{
-//             console.log('ERR: ', error);
-//             throw error
-//         })
-
-//         app.listen(process.env.PORT, ()=>{
-//             console.log(`LISTENING ON PORT ${process.env.PORT}`);
-//         })
-//     } catch(error){
-//         console.log(`Error: ${error}`);
-//         throw error
-//     }
-// })()
-//the other way ...
-// we create a connection instance in a different dir made for db handling and export it
